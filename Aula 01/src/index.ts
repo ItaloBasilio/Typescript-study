beece5ae6a420397939f5c6580c1938feb1cda37
ItaloBasilio <italobasiliodasilva@gmail.com>
2024-03-25 08:07:39 -0300

    update

--- Aula 01/src/index.ts
+++
@@ -1,7 +0,0 @@
-function sum(a: number,b: number): number{
-    return a + b;
-}
-
-const result = sum(2,1) 
-
-console.log(result);
---
+++ Aula01/src/static-typing.ts
@@ -0,0 +1,8 @@
+/**
+ *  Tipos primitivos
+ *  Number => inteiros, decimais
+ *  string => textos, caracteres
+ *  boolean => true ou false
+ */
+
+let age: number = 22; 
